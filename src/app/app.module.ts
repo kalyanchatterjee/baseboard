be8fd56e70456bd4eefe7587dd1f36502438fbfc
Kalyan Chatterjee <Kalyan.Chatterjee@cantire.com>
2019-07-18 16:28:56 -0400

    Cleaned master branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './component/header/header.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, NavbarComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

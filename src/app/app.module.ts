e0da33d212ca958ddb344b11d71f7cce8261ae27
Inna Chernov <Inna.Chernov@cantire.com>
2019-07-18 14:05:54 -0400

    added grid to main coponent HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './component/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/Main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule

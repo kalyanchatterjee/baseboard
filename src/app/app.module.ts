eadbfd68deae83b21cc90fa1abd7858718417bc3
Kalyan Chatterjee <Kalyan.Chatterjee@cantire.com>
2019-07-19 09:37:24 -0400

    Fixed Main vs main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
-import { MainComponent } from "./components/Main/main.component";
+import { MainComponent } from "./components/main/main.component";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, MainComponent],

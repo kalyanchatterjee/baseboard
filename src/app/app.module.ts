676f281b8a720ab87d381448a39af74f353c4806
Kalyan Chatterjee <Kalyan.Chatterjee@cantire.com>
2019-07-19 09:22:16 -0400

    First commit day 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
-<<<<<<< HEAD
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
-import { NavbarComponent } from "./components/navbar/navbar.component";
+import { MainComponent } from "./components/Main/main.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NavbarComponent],
+  declarations: [AppComponent, HeaderComponent, MainComponent],
   imports: [BrowserModule],
-=======
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { MainComponent } from './components/Main/main.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    MainComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
->>>>>>> inna
   providers: [],
   bootstrap: [AppComponent]
 })

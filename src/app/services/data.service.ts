25c4ca068f51db0d97fc24a309d6308b8a4455d5
Kalyan Chatterjee <Kalyan.Chatterjee@cantire.com>
2019-07-19 14:33:39 -0400

    Created Card data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Card } from "../models/Card";
+
+const httpOptions = {
+  headers: new HttpHeaders({ "Content-Type": "applicaiton/json" })
+};
 
 @Injectable({
   providedIn: "root"
 })
 export class DataService {
-  constructor() {}
+  postUrl: string = "http://10.126.224.34:3000/api/data";
+  constructor(private http: HttpClient) {}
+
+  getCardsData(): Observable<Card[]> {
+    return this.http.get<Card[]>(this.postUrl);
+  }
 }

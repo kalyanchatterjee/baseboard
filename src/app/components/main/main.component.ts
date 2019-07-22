f71b9ab3588bfe41f6204b61f684d3189bbe8ab1
Kalyan Chatterjee <Kalyan.Chatterjee@cantire.com>
2019-07-22 00:09:09 -0400

    Able to pass data from Main to Cards

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,27 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../../services/data.service';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../../services/data.service";
 
-import { CardComponent } from '../card/card.component';
-import { Card } from '../../models/Card'
-
+import { CardComponent } from "../card/card.component";
+import { Card } from "../../models/Card";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.css"]
 })
 export class MainComponent implements OnInit {
-
-  constructor( private dataService: DataService) { }
-  cards: Card[]
+  constructor(private dataService: DataService) {}
+  cards: Card[];
   ngOnInit() {
     // this.dataService.getCardsData().subscribe(cards =>{
     //   this.cards=cards;
     //   console.log(this.cards)
     // })
-    this.cards = this.dataService.getCardsData()
-    console.log(this.cards)
-    
+    this.cards = this.dataService.getCardsData();
   }
-
 }
